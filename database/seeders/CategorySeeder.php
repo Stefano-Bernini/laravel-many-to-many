05023eb3bc7d77bcfcac49b319aea5fa8488d3f4
Stefano-Bernini <stefanobernini26@gmail.com>
2023-09-01 02:55:56 +0200

    first commit

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+use Illuminate\Support\Str;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['Frontend', 'Backend', 'Fullstack', 'Design', 'DevOps'];
+
+        foreach($categories as $cat){
+            $category = new Category();
+
+            $category->name = $cat;
+            $category->slug = Str::slug($cat, '-');
+
+            $category->save();
+        }
+    }
+}
